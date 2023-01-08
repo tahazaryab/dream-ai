1cf90924a32438ffcf7f527bb07e527dd7f3572a
Taha Zaryab <tahazaryab10@gmail.com>
2023-01-07 21:40:58 -0500

    Install and configure prettier + format code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>DreamReel</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>DreamReel</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
