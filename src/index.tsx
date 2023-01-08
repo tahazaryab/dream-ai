3d24d6d929516a6175642cfb7519676051cf6fec
Taha Zaryab <tahazaryab10@gmail.com>
2023-01-07 21:45:20 -0500

    Update README

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import App from './App'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
